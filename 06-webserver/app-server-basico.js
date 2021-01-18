b6e45046ceab1ed57bf8c637e91a09dc80b97788
Jose Signoret <jsignoretfernandez@gmail.com>
2021-01-18 01:30:23 +0100

    06 Finish WebServer

---
+++ 06-webserver/app-server-basico.js
@@ -0,0 +1,24 @@
+
+const http = require('http');
+
+http.createServer( ((req, res) => {
+
+    res.writeHead(200, {
+        'content-type': 'application/json'
+    });
+
+    let salida = {
+        nombre: 'Jose',
+        edad: 35,
+        url: req.url
+    }
+
+    res.write(JSON.stringify(salida));
+    //res.write('Hola Mundo');
+    res.end();
+
+}))
+.listen(8080);
+
+
+console.log('Escuchando el puerto 8080')
